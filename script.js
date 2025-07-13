4cc8d363bbf2dac4889362074c0f3003a1ab9934
Priyangshu39 <priyangshu_bit2024@msit.edu.in>
2025-07-13 16:09:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+document.getElementById('nextBtn1').addEventListener('click', function() {
+  document.getElementById('slide1').classList.add('hidden');
+  document.getElementById('slide2').classList.remove('hidden');
+});
+
+document.getElementById('nextBtn2').addEventListener('click', function() {
+  document.getElementById('slide2').classList.add('hidden');
+  document.getElementById('slide3').classList.remove('hidden');
+  confetti({
+    particleCount: 200,
+    spread: 70,
+    origin: { y: 0.6 }
+  });
+});
