7bccc0548892c20a3d9649c971dd52ac2de79866
Priyangshu39 <priyangshu_bit2024@msit.edu.in>
2025-07-13 16:18:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const bgMusic = document.getElementById('bgMusic'); // <<== IMPORTANT
 
 document.getElementById('nextBtn1').addEventListener('click', function() {
   document.getElementById('slide1').classList.add('hidden');
   document.getElementById('slide2').classList.remove('hidden');
+  bgMusic.play(); // <<== START MUSIC on click
 });
 
 document.getElementById('nextBtn2').addEventListener('click', function() {
